0d62fd3e6b93bcc73389fcd220345e34f2974e87
Parth Singh <parth3743@gmail.com>
2024-06-15 17:51:55 +0530

    Backend: Added Product Schema

--- backend/src/schema/typedef.ts
+++ backend/src/schema/typedef.ts
@@ -1,4 +1,14 @@
 export const typeDefs = `#graphql
+
+    enum ProductCategory {
+        TerraCotta
+        Macrame
+        Moonj
+        BananaFiber
+        JuteBags
+        Other
+    }
+
     type User {
         id: ID!
         firstName: String!
@@ -7,5 +17,19 @@ export const typeDefs = `#graphql
         profileImageURL: String
         phoneNo: String
         userRole: String
+    }
+
+    type Product {
+        id: ID!
+        name: String!
+        SKU_ID: String!
+        height: Float
+        weight: Float
+        quantity: Int
+        imageURL: String
+        description: String
+        userId: String!
+        user: User!
+        category: ProductCategory!
     }
 `;

cd111a1b7b1375a2c6011affe3bad878009423b5
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-06-15 14:58:38 +0530

    Frontend Boilerplate

---
+++ frontend/app/catalog/home/page.tsx
@@ -0,0 +1,7 @@
+export default function CatalogHomePage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Catalog Home page
+        </main>
+    );
+}
---
+++ frontend/app/clusterAdmin/page.tsx
@@ -0,0 +1,7 @@
+export default function ClusterAdminPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Cluster Admin page
+        </main>
+    );
+}
